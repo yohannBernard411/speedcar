1c5a3d92896db89732fbd26fe083526b7a80c212
yohber <yohannb215@gmail.com>
2024-03-05 00:00:52 +0100

    test ssh push to hostinger2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'speedcar'`, () => {
+  it(`should have as title 'My big speedcar'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('speedcar');
+    expect(app.title).toEqual('My big speedcar');
   });
 
   it('should render title', () => {
